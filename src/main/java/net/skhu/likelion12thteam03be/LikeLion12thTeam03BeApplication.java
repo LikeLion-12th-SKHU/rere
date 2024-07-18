77198d7fa8d0f4a6c7dc42400cd840af9159f4a7
shinheekim <shinhee1241@gmail.com>
2024-07-18 22:50:22 +0900

    build: 프로젝트 생성 및 yml파일 설정

---
+++ src/main/java/net/skhu/likelion12thteam03be/LikeLion12thTeam03BeApplication.java
@@ -0,0 +1,13 @@
+package net.skhu.likelion12thteam03be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeLion12thTeam03BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikeLion12thTeam03BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/skhu/likelion12thteam03be/LikeLion12thTeam03BeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.skhu.likelion12thteam03be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeLion12thTeam03BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
