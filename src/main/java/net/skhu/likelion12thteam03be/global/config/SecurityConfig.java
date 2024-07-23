966b364f60bb744876b9839ecbb427dd55e3aba3
shinheekim <shinhee1241@gmail.com>
2024-07-23 14:11:09 +0900

    refactor: 웹 애플리케이션 보안 설정 수정

--- src/main/java/net/skhu/likelion12thteam03be/global/config/SecurityConfig.java
+++ src/main/java/net/skhu/likelion12thteam03be/global/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package net.skhu.likelion12thteam03be.global.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.skhu.likelion12thteam03be.global.jwt.JwtAuthorizationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Slf4j
 @Configuration  // @Bean을 사용하는 클래스에는 반드시 @Configuration -> 해당 클래스에서 Bean을 등록하고자 함을 명시
 @EnableWebSecurity  // 위랑 같이 쓰이는 편. 자동으로 스프링 시큐리티 필터 체인 생성하고 웹 보안을 활성화
 @RequiredArgsConstructor
@@ -20,15 +23,23 @@ public class SecurityConfig {
     private final JwtAuthorizationFilter jwtAuthorizationFilter;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {    //SecurityFilterChain
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/users/**").permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/csrf-token").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users/join").permitAll()  // 모든 사용자 회원가입 엔드포인트 허용
+                        .requestMatchers(HttpMethod.POST, "/users/login").permitAll() // 모든 사용자 회원가입 엔드포인트 허용
+                        .requestMatchers(HttpMethod.GET, "/users").authenticated()
+                        .anyRequest().permitAll()
                 )
                 .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
-                .sessionManagement(sessionManagement -> sessionManagement   //세션 관리 정책. 토큰 관리하겠다.
+                .sessionManagement(sessionManagement -> sessionManagement
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
