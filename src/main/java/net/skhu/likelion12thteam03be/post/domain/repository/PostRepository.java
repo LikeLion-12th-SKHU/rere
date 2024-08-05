75e639b37e45ed30c49cba2978fee84a7570834e
firefox1234123 <dayun01004@gmail.com>
2024-08-05 20:11:19 +0900

    feat: 글 검색 조회 구현#3

--- src/main/java/net/skhu/likelion12thteam03be/post/domain/repository/PostRepository.java
+++ src/main/java/net/skhu/likelion12thteam03be/post/domain/repository/PostRepository.java
@@ -13,6 +13,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Query("select distinct p from Post p join fetch p.moods m where m.moodId = :moodId ")
     List<Post> findByMoodId(Long moodId);
 
-    @Query("select distinct p from Post p where p.title like :input or p.content like :input")
+    @Query("select distinct p from Post p where p.title like :input or p.content like :input ")
     List<Post> findByInput(String input);
 }
